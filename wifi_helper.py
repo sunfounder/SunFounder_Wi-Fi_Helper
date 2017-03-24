c7f4177e5415b3194fc55e7643c50878d6645370
Cavon <lijiarong@sunfounder.com>
2017-03-24 17:43:08 +0800

    add debug messages re-oganizedthe script

--- wifi_helper.py
+++ wifi_helper.py
@@ -1,202 +1,240 @@
 #!/usr/bin/python
 
-from tkinter import *
-import string
-import os
-import _thread
-import socket
-from platform import system as system_name # Returns the system/OS name
-from os import system as system_call       # Execute a shell command
 
-VERSION = "v1.0.0"
-COMPANY = "SunFounder     www.sunfounder.com"
+class WiFiHelper(object):
+    import tkinter as tk
+    import string
+    import os
+    import _thread
+    import socket
+    from platform import system as system_name # Returns the system/OS name
+    from os import system as system_call       # Execute a shell command
+    import time
 
-bg = '#EEEEEE'
-fg = '#202020'
-ErrorColor = '#FA0F0F'
+    VERSION = "v1.1.0"
+    COMPANY = "SunFounder     www.sunfounder.com"
 
-alphabet = list(string.ascii_lowercase)
+    BACKGROUND_COLOR = '#EEEEEE'
+    FOREGROUND_COLOR = '#202020'
+    ERROR_COLOR = '#FA0F0F'
 
-def setup():
-    global top, message, ip_listbox, disk_menu, frame_2
-    global ssid_input, passwd_input, enabled_ssh, disk, disks
+    _alphabets = list(string.ascii_lowercase)
 
-    # Get avalible disks
-    disks = get_all_disk()
+    def __init__(self):
+        # Get avalible disks
+        disks = self.get_all_disk()
+        self.top = self.tk.Tk()
+        self.top.title('SunFounder Wi-Fi Helper')
+        self.top.iconbitmap('./SunFounder_LOGO_small.ico')
+        main_frame = self.tk.Frame(self.top, width=800)
+        info_frame = self.tk.Frame(self.top)
+        control_frame = self.tk.Frame(main_frame, width=360)
+        debug_frame = self.tk.Frame(main_frame, width=360)
+        frame_1_1 = self.tk.Frame(control_frame)
+        self.frame_1_2 = self.tk.Frame(control_frame)
+        frame_1_3 = self.tk.Frame(control_frame)
+        frame_1_4 = self.tk.Frame(control_frame)
+        frame_1_5 = self.tk.Frame(control_frame)
+        frame_1_6 = self.tk.Frame(control_frame)
+        frame_1_7 = self.tk.Frame(control_frame)
+        frame_1_8 = self.tk.Frame(control_frame)
+        frame_1_9 = self.tk.Frame(control_frame)
+        frame_2_1 = self.tk.Frame(debug_frame)
+        frame_2_2 = self.tk.Frame(debug_frame)
+        self.enabled_ssh = self.tk.IntVar()
+        self.disk = self.tk.StringVar(self.frame_1_2)
+        self.message = self.tk.StringVar(frame_1_7)
+        self.message.set('')
+        self.disk.set('')
 
-    top = Tk()
-    top.title('SunFounder Wi-Fi Helper')
-    top.iconbitmap('./SunFounder_LOGO_small.ico')
-    main_frame = Frame(top)
-    info_frame = Frame(top)
-    control_frame = Frame(main_frame)
-    debug_frame = Frame(main_frame)
-    frame_1 = Frame(control_frame)
-    frame_2 = Frame(control_frame)
-    frame_3 = Frame(control_frame)
-    frame_4 = Frame(control_frame)
-    frame_5 = Frame(control_frame)
-    frame_6 = Frame(control_frame)
-    frame_7 = Frame(control_frame)
-    frame_8 = Frame(control_frame)
-    frame_9 = Frame(control_frame)
-    enabled_ssh = IntVar()
-    disk = StringVar(frame_2)
-    message = StringVar(frame_7)
-    message.set('')
-    disk.set('')
-
-    main_lable     = Label(frame_1, text="Raspbian Wi-Fi Helper", fg=fg)
-    disk_label     = Label(frame_2, text="Select the disk of your Rasbian", fg=fg)
-    disk_menu      = OptionMenu(frame_2, disk, *disks)
-    disk_reflash   = Button(frame_2, width=7, text='Reflash')
-    ssid_label     = Label(frame_3, text="SSID: ", fg=fg)
-    ssid_input     = Entry(frame_3, bg=bg, fg=fg)
-    passwd_label   = Label(frame_4, text="PASSWORD: ", fg=fg)
-    passwd_input   = Entry(frame_4, bg=bg, fg=fg)
-    ssh_check      = Checkbutton(frame_5, text="Enable SSH", variable=enabled_ssh)
-    message_lable  = Label(frame_6, textvariable=message, fg=ErrorColor)
-    confirm_button = Button(frame_7, width=10, text='Confirm')
-    search_button  = Button(frame_8, width=20, text='Search Raspberry Pi')
-    ip_listbox     = Listbox(frame_9, fg=fg)
-    debug_lable    = Listbox(debug_frame, fg=fg)
-    info_version   = Label(info_frame,text=VERSION, fg=fg)
-    info_company   = Label(info_frame,text=COMPANY, fg=fg)
+        main_lable         = self.tk.Label(frame_1_1, text="Raspbian Wi-Fi Helper", fg=self.FOREGROUND_COLOR)
+        disk_label         = self.tk.Label(self.frame_1_2, text="Select the disk of your Rasbian", fg=self.FOREGROUND_COLOR)
+        self.disk_menu     = self.tk.OptionMenu(self.frame_1_2, self.disk, *disks)
+        disk_reflash       = self.tk.Button(self.frame_1_2, width=7, text='Reflash')
+        ssid_label         = self.tk.Label(frame_1_3, text="SSID: ", fg=self.FOREGROUND_COLOR)
+        self.ssid_input         = self.tk.Entry(frame_1_3, bg=self.BACKGROUND_COLOR, fg=self.FOREGROUND_COLOR)
+        passwd_label       = self.tk.Label(frame_1_4, text="PASSWORD: ", fg=self.FOREGROUND_COLOR)
+        self.passwd_input       = self.tk.Entry(frame_1_4, bg=self.BACKGROUND_COLOR, fg=self.FOREGROUND_COLOR)
+        ssh_check          = self.tk.Checkbutton(frame_1_5, text="Enable SSH", variable=self.enabled_ssh)
+        message_lable      = self.tk.Label(frame_1_6, textvariable=self.message, fg=self.ERROR_COLOR)
+        confirm_button     = self.tk.Button(frame_1_7, width=10, text='Confirm')
+        search_button      = self.tk.Button(frame_1_8, width=20, text='Search Raspberry Pi')
+        self.ip_listbox    = self.tk.Listbox(frame_1_9, width=50, height=200)
+        debug_label        = self.tk.Label(frame_2_1, text="Debug:", fg=self.FOREGROUND_COLOR)
+        self.debug_listbox = self.tk.Listbox(frame_2_2, width=100, height=200)
+        info_version       = self.tk.Label(info_frame,text=self.VERSION, fg=self.FOREGROUND_COLOR)
+        info_company       = self.tk.Label(info_frame,text=self.COMPANY, fg=self.FOREGROUND_COLOR)
 
 
-    # Pack 
-    main_frame.pack(padx=20, pady=20, fill="both")
-    info_frame.pack(side=BOTTOM,fill="both")
-    control_frame.pack(side=LEFT, fill="both")
-    debug_frame.pack(side=RIGHT, fill="both")
-    frame_1.pack(pady=10, side=TOP,fill="both")
-    frame_2.pack(pady=10, side=TOP,fill="both")
-    frame_3.pack(padx=30, side=TOP,fill="both")
-    frame_4.pack(padx=30, side=TOP,fill="both")
-    frame_5.pack(side=TOP,fill="both")
-    frame_6.pack(side=TOP,fill="both")
-    frame_7.pack(side=TOP,fill="both")
-    frame_8.pack(side=TOP,fill="both")
-    frame_9.pack(side=TOP,fill="both")
-    main_lable.pack(side=LEFT)
-    disk_label.pack(side=LEFT)
-    disk_reflash.pack(side=RIGHT)
-    disk_menu.pack(side=RIGHT)
-    ssid_label.pack(side=LEFT)
-    ssid_input.pack(side=RIGHT)
-    passwd_label.pack(side=LEFT)
-    passwd_input.pack(side=RIGHT)
-    ssh_check.pack()
-    message_lable.pack(side=LEFT)
-    confirm_button.pack(side=RIGHT)
-    search_button.pack(side=LEFT)
-    ip_listbox.pack(fill="both")
-    info_version.pack(side=LEFT)
-    info_company.pack(side=RIGHT)
+        # Pack 
+        main_frame.pack(fill="both")
+        info_frame.pack(side=self.tk.BOTTOM,fill="both")
 
-    # Setup
-    top.resizable(width=False, height=False)
-    top.geometry('{}x{}'.format(800, 480))
-    confirm_button.bind('<ButtonRelease-1>', confirm)
-    search_button.bind('<ButtonRelease-1>', search)
-    disk_reflash.bind('<ButtonRelease-1>', reflash)
-    ssh_check.select()
-    ip_listBox_.config(state=DISABLED)
+        control_frame.pack(side=self.tk.LEFT, padx=20, pady=20, fill="both")
+        debug_frame.pack(side=self.tk.RIGHT, padx=20, pady=20, fill="both")
 
-def main():
-    top.mainloop()
+        frame_1_1.pack(pady=10, side=self.tk.TOP,fill="both")
+        self.frame_1_2.pack(side=self.tk.TOP,fill="both")
+        frame_1_3.pack(padx=30, side=self.tk.TOP,fill="both")
+        frame_1_4.pack(padx=30, side=self.tk.TOP,fill="both")
+        frame_1_5.pack(side=self.tk.TOP,fill="both")
+        frame_1_6.pack(side=self.tk.TOP,fill="both")
+        frame_1_7.pack(side=self.tk.TOP,fill="both")
+        frame_1_8.pack(side=self.tk.TOP,fill="both")
+        frame_1_9.pack(side=self.tk.TOP,fill="both")
 
-def quit_fun(event):
-    top.quit()
+        frame_2_1.pack(side=self.tk.TOP,fill="x")
+        frame_2_2.pack(side=self.tk.TOP,fill="both")
 
-def reflash(event):
-    global disk_menu
-    disk_menu.pack_forget()
-    disks = get_all_disk()
-    disk_menu = OptionMenu(frame_2, disk, *disks)
-    disk_menu.pack(side=RIGHT)
+        main_lable.pack(side=self.tk.LEFT)
+        disk_label.pack(side=self.tk.LEFT)
+        disk_reflash.pack(side=self.tk.RIGHT)
+        self.disk_menu.pack(side=self.tk.RIGHT)
+        ssid_label.pack(side=self.tk.LEFT)
+        self.ssid_input.pack(side=self.tk.RIGHT)
+        passwd_label.pack(side=self.tk.LEFT)
+        self.passwd_input.pack(side=self.tk.RIGHT)
+        ssh_check.pack()
+        message_lable.pack(side=self.tk.LEFT)
+        confirm_button.pack(side=self.tk.RIGHT)
+        search_button.pack(side=self.tk.LEFT)
+        self.ip_listbox.pack(fill="x")
 
-def confirm(event):
-    if ssid_input.get() == "":
-        message.set("Error! SSID must not be empty!")
-    elif passwd_input.get() == "":
-        message.set("Error! PASSWARD must not be empty!")
-    else:
-        wpa = 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev,update_config=1,,network={,    ssid="%s",    psk="%s",    key_mgmt=WPA-PSK,}' % (ssid_input.get(),passwd_input.get())
-        wpa = wpa.replace(',', '\n')
-        print("wpa_file:\n%s"%wpa)
-        if apply_settings(wpa):
-            message.set("Done!")
+        debug_label.pack(side=self.tk.LEFT)
+        self.debug_listbox.pack(fill="both")
 
-def get_all_disk():
-    disks = []
-    for disk in alphabet:
-        disk = '%s:'%disk
+        info_version.pack(side=self.tk.LEFT)
+        info_company.pack(side=self.tk.RIGHT)
+
+        # Setup
+        self.top.resizable(width=False, height=False)
+        self.top.geometry('{}x{}'.format(1000, 480))
+        confirm_button.bind('<ButtonRelease-1>', self.confirm)
+        search_button.bind('<ButtonRelease-1>', self.search)
+        disk_reflash.bind('<ButtonRelease-1>', self.reflash)
+        ssh_check.select()
+        #self.ip_listbox.config(state=self.tk.DISABLED)
+        #self.debug_listbox.config(state=self.tk.DISABLED)
+
+    def start(self):
+        self.debug("Ready.")
+        self.top.mainloop()
+
+    def get_all_disk(self):
+        disks = []
+        for disk in self._alphabets:
+            disk = '%s:'%disk
+            try:
+                self.os.listdir(disk)
+                disks.append(disk)
+            except:
+                continue
+        return disks
+
+    def reflash(self, event):
+        self.debug("Reflashing avalible disks...")
+        self.disk_menu.pack_forget()
+        disks = self.get_all_disk()
+        self.disk_menu = self.tk.OptionMenu(self.frame_1_2, self.disk, *disks)
+        self.disk_menu.pack(side=self.tk.RIGHT)
+        self.debug("Done!")
+
+    def confirm(self, event):
+        if self.disk.get() == "":
+            self.message.set("Error! You must select a disk!")
+            self.debug("Error! You must select a disk!")
+        elif self.ssid_input.get() == "":
+            self.message.set("Error! SSID must not be empty!")
+            self.debug("Error! SSID must not be empty!")
+        elif self.passwd_input.get() == "":
+            self.message.set("Error! PASSWARD must not be empty!")
+            self.debug("Error! PASSWARD must not be empty!")
+        else:
+            wpa = 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev,update_config=1,,network={,    ssid="%s",    psk="%s",    key_mgmt=WPA-PSK,}' % (self.ssid_input.get(),self.passwd_input.get())
+            wpa = wpa.replace(',', '\n')
+            self.debug("wpa_file:\n%s"%wpa)
+            if self.apply_settings(wpa):
+                self.message.set("Done!")
+                self.debug("Done!")
+                self.debug("You can now unmount your TF Card.")
+                self.debug("And turn on your Pi.")
+
+
+    def apply_settings(self, wpa):
+        wpa_file = "%s/wpa_supplicant.conf" % self.disk.get()
+        ssh_file = "%s/ssh" % self.disk.get()
+        self.debug("wpa file will be at: %s" % wpa_file)
+        self.debug("ssh file will be at: %s" % ssh_file)
+        
+        f = open(wpa_file, 'w')
+        f.write(wpa)
+        f.close()
+        if self.enabled_ssh.get():
+            f = open(ssh_file, 'w')
+            f.write('')
+            f.close
+        return 1
+
+    def search(self, event):
+        self.debug("Searching...")
+        self.ip_listbox.delete(0, self.tk.END)
+        my_name = self.socket.getfqdn(self.socket.gethostname())
+        my_address = self.socket.gethostbyname(my_name)
+        gateway = my_address.split('.')
+        gateway = gateway[0]+'.'+gateway[1]+'.'+gateway[2]+'.'
+        self.debug('My Name: %s' % my_name)
+        self.debug('My Address: %s' % my_address)
+        self.debug('Gateway: %s' % gateway+'0')
+        self.count = 0
+        for i in range(1,255):
+            ip = gateway+'%d'%i
+            self._thread.start_new_thread(self.search_ip, (ip,))
+            #search_ip(ip)
+        self.debug('Done!')
+        if self.count == 0:
+            self.debug("No Raspberry Pi found.")
+        else:
+            self.debug("Found %d Raspberry Pi" % self.count)
+
+    def search_ip(self, ip):
         try:
-            os.listdir(disk)
-            disks.append(disk)
+            host = self.socket.gethostbyaddr(ip)
+            host = host[0].split('.')[0]
+            if host == 'raspberrypi':
+                status = ping(ip)
+                self.debug(status)
+                if status:
+                    self.debug('host: %s@%s' % (host,ip))
+                    self.ip_listbox.insert(self.tk.END, '%s@%s' % (host,ip))
+                    count += 1
         except:
-            continue
-    return disks
-
-def apply_settings(wpa):
-    wpa_file = "%s/wpa_supplicant.conf" % disk.get()
-    ssh_file = "%s/ssh" % disk.get()
-    print("wpa file will be at: %s" % wpa_file)
-    print("ssh file will be at: %s" % ssh_file)
-    
-    f = open(wpa_file, 'w')
-    f.write(wpa)
-    f.close()
-    if enabled_ssh.get():
-        f = open(ssh_file, 'w')
-        f.write('')
-        f.close
-    return 1
-
-def search(event):
-    ip_listbox.delete(0, END)
-    my_name = socket.getfqdn(socket.gethostname())
-    my_address = socket.gethostbyname(my_name)
-    gateway = my_address.split('.')
-    gateway = gateway[0]+'.'+gateway[1]+'.'+gateway[2]+'.'
-    print('My Name: %s' % my_name)
-    print('My Address: %s' % my_address)
-    print('Gateway: %s' % gateway+'0')
-    for i in range(1,255):
-        ip = gateway+'%d'%i
-        _thread.start_new_thread(search_ip, (ip,))
-        #search_ip(ip)
-
-def search_ip(ip):
-    try:
-        host = socket.gethostbyaddr(ip)
-        host = host[0].split('.')[0]
-        if host == 'raspberrypi':
-            status = ping(ip)
-            print(status)
-            if status:
-                print('host: %s@%s' % (host,ip))
-                ip_listbox.insert(END, '%s@%s' % (host,ip))
-    except:
-        pass
-        #print("Unexpected error: %s" % sys.exc_info()[0])
+            pass
+            #self.debug("Unexpected error: %s" % sys.exc_info()[0])
 
 
-def ping(host):
-    """
-    Returns True if host (str) responds to a ping request.
-    Remember that some hosts may not respond to a ping request even if the host name is valid.
-    """
+    def ping(self, host):
+        """
+        Returns True if host (str) responds to a ping request.
+        Remember that some hosts may not respond to a ping request even if the host name is valid.
+        """
 
-    # Ping parameters as function of OS
-    parameters = "-n 1" if system_name().lower()=="windows" else "-c 1"
+        # Ping parameters as function of OS
+        parameters = "-n 1" if system_name().lower()=="windows" else "-c 1"
 
-    # Pinging
-    return system_call("ping " + parameters + " " + host) == 0
+        # Pinging
+        return system_call("ping " + parameters + " " + host) == 0
+
+    def debug(self, messages):
+        localtime = self.time.asctime(self.time.localtime(self.time.time()))
+        messages = messages.split('\n')
+        for m in messages:
+            m = str(localtime) + "    " + m
+            print(m)
+            self.debug_listbox.insert(self.tk.END, "%s"%m)
+            self.debug_listbox.yview(self.tk.END)
 
 if __name__ == '__main__':
-    setup()
-    main()
+    w = WiFiHelper()
+    w.start()
 
 
